83e2223fe2e7405a9ec5373716c85c840936b9ab
vpred <vpreduna90@gmail.com>
2021-08-11 00:33:37 -0700

    +Pages.js, data for pages displayed on menu; #4

---
+++ src/data/Pages.js
@@ -0,0 +1,35 @@
+// import { GiAncientColumns, GiTransportationRings } from 'react-icons/gi';
+// import { AiOutlineContacts, AiOutlineOrderedList } from 'react-icons/ai';
+// AiOutlineEye
+
+const pages = [
+	{
+		id: 1,
+		status: 'hidden',
+		title: 'Interact',
+		pageUrl: '',
+		icon: 'ICON'// <GiTransportationRings />,
+	},
+	{
+		id: 2,
+		status: 'hidden',
+		title: 'About',
+		pageUrl: '',
+		icon: 'ICON'// <AiOutlineOrderedList />,
+	},
+	{
+		id: 3,
+		status: 'hidden',
+		title: 'Contact',
+		pageUrl: '',
+		icon: 'ICON'// <AiOutlineContacts />,
+	},
+	{
+		id: 4,
+		status: 'hidden',
+		title: 'Portfolio',
+		pageUrl: '',
+		icon: 'ICON'// <GiAncientColumns />,
+	},
+];
+export default pages;
