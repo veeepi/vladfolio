f09991a1895c32e465246002942e7a3c64fb7a05
vpred <vpreduna90@gmail.com>
2021-08-14 14:06:26 -0700

    display PagePanel; #3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './styles/main.scss';
 import './App.css';
+import React, { useState } from 'react';
 import RadialMenu from './components/menu/RadialMenu';
+import PagePanel from './components/panel/PagePanel';
+import pages from './data/Pages';
 
 function App() {
+  const [pageDisplayed, setPageDisplayed] = useState(pages[0]);
+
   return (
     <div className="App">
       <RadialMenu />
+      <PagePanel pageDisplayed={pageDisplayed} />
     </div>
   );
 }
