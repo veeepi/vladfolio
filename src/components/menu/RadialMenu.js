88b9f17e4882e2357914f50bc6f2a8bdd7af0863
vpred <vpreduna90@gmail.com>
2021-08-14 12:57:13 -0700

    fix className 'radialMenu' to 'outer'; #3

--- src/components/menu/RadialMenu.js
+++ src/components/menu/RadialMenu.js
@@ -2,22 +2,22 @@ import React from 'react';
 import Quadrant from './Quadrant';
 import pages from '../../data/Pages';
 
-export default function RadialMenu({}) {
+export default function outer({}) {
     return (
-        <div className="radialMenu">
-            <div className="radialMenu__quadrant" id="top_left_quadrant">
+        <div className="outer">
+            <div className="outer__quadrant" id="top_left_quadrant">
                 <Quadrant pageId={pages[0].id} />
             </div>
             {/* ------------------------------- */}
-            <div className="radialMenu__quadrant" id="top_right_quadrant">
+            <div className="outer__quadrant" id="top_right_quadrant">
                 <Quadrant pageId={pages[1].id} />
             </div>
             {/* ------------------------------- */}
-            <div className="radialMenu__quadrant" id="bottom_left_quadrant">
+            <div className="outer__quadrant" id="bottom_left_quadrant">
                 <Quadrant pageId={pages[2].id} />
             </div>
             {/* ------------------------------- */}
-            <div className="radialMenu__quadrant" id="bottom_right_quadrant">
+            <div className="outer__quadrant" id="bottom_right_quadrant">
                 <Quadrant pageId={pages[3].id} />
             </div>
         </div>
