a85ef9d493ba3472c873334dc71e32076c3d7dc0
vpred <vpreduna90@gmail.com>
2021-08-15 09:38:21 -0700

    Panel toggle statevar, func, useEffect; #3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import './styles/main.scss';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import RadialMenu from './components/menu/RadialMenu';
 import PagePanel from './components/panel/PagePanel';
 import pages from './data/Pages';
 
 function App() {
+  // state var
+  const [displayPagePanel, setDisplayPagePanel] = useState(false); // should update after via useEffect Hook
   const [pageDisplayed, setPageDisplayed] = useState(pages[0]);
+
+  useEffect(() => {
+    // when pageDisplayed changes
+    if (pageDisplayed !== pages[0]) {
+      // AND page=nonzero,
+      toggleDisplayPagePanel(); // toggle display of the Panel
+    }
+  }, [pageDisplayed]);
+
+  // functions
+  const toggleDisplayPagePanel = () => {
+    setDisplayPagePanel((prevDisplayPagePanel) => !prevDisplayPagePanel);
+  };
 
   return (
     <div className="App">
       <RadialMenu />
-      <PagePanel pageDisplayed={pageDisplayed} />
+
+      {displayPagePanel /*on &&*/ && (
+        <PagePanel pageDisplayed={pageDisplayed} />
+      )}
     </div>
   );
 }
