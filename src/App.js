003ef5d826c4c51cfed0bb259dbb0f7e9a5f8695
vpred <vpreduna90@gmail.com>
2021-08-15 10:39:50 -0700

    createContext: DragContext on RadialMenu; #9

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './styles/main.scss';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import RadialMenu from './components/menu/RadialMenu';
 import PagePanel from './components/panel/PagePanel';
 import pages from './data/Pages';
+
+export const DragContext = createContext();
 
 function App() {
   // state var
   const [displayPagePanel, setDisplayPagePanel] = useState(false); // should update after via useEffect Hook
   const [pageDisplayed, setPageDisplayed] = useState(pages[0]);
+  const [aButtonIsDragging, setAButtonIsDragging] = useState(false); // for reactive styling
 
   useEffect(() => {
     // when pageDisplayed changes
@@ -22,14 +25,26 @@ function App() {
   const toggleDisplayPagePanel = () => {
     setDisplayPagePanel((prevDisplayPagePanel) => !prevDisplayPagePanel);
   };
+  const updateSelectedPage = (page) => {
+    setPageDisplayed(page);
+  };
+
+  // function triggered by DragButton's isDragging property
+  const toggleAButtonIsDragging = () => {
+    setAButtonIsDragging((prevAButtonIsDragging) => !prevAButtonIsDragging);
+  };
 
   return (
     <div className="App">
       <RadialMenu />
 
-      {displayPagePanel /*on &&*/ && (
-        <PagePanel pageDisplayed={pageDisplayed} />
-      )}
+      <DragContext.Provider
+        value={{ toggleAButtonIsDragging, updateSelectedPage }}
+      >
+        {displayPagePanel /*on &&*/ && (
+          <PagePanel pageDisplayed={pageDisplayed} />
+        )}
+      </DragContext.Provider>
     </div>
   );
 }
