b9ded0394012dea81b963c369a7597130719f105
vpred <vpreduna90@gmail.com>
2021-08-15 11:49:02 -0700

    consume DragTarget in RadialMenu; #9

--- src/components/menu/RadialMenu.js
+++ src/components/menu/RadialMenu.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import Quadrant from './Quadrant';
+import DragTarget from '../dnd/DragTarget';
 import pages from '../../data/Pages';
 
-export default function outer({}) {
-    return (
-        <div className="outer">
-            <div className="outer__quadrant" id="top_left_quadrant">
-                <Quadrant pageId={pages[0].id} />
-            </div>
-            {/* ------------------------------- */}
-            <div className="outer__quadrant" id="top_right_quadrant">
-                <Quadrant pageId={pages[1].id} />
-            </div>
-            {/* ------------------------------- */}
-            <div className="outer__quadrant" id="bottom_left_quadrant">
-                <Quadrant pageId={pages[2].id} />
-            </div>
-            {/* ------------------------------- */}
-            <div className="outer__quadrant" id="bottom_right_quadrant">
-                <Quadrant pageId={pages[3].id} />
-            </div>
-        </div>
-    );
+export default function RadialMenu({ aButtonIsDragging }) {
+  return (
+    <div className="outer">
+      <div className="outer__quadrant" id="top_left_quadrant">
+        <Quadrant pageId={pages[0].id} />
+      </div>
+      {/* ------------------------------- */}
+      <div className="outer__quadrant" id="top_right_quadrant">
+        <Quadrant pageId={pages[1].id} />
+      </div>
+      {/* ------------------------------- */}
+      <div className="outer__quadrant" id="bottom_left_quadrant">
+        <Quadrant pageId={pages[2].id} />
+      </div>
+      {/* ------------------------------- */}
+      <div className="outer__quadrant" id="bottom_right_quadrant">
+        <Quadrant pageId={pages[3].id} />
+      </div>
+      <div className="inner">
+        <DragTarget aButtonIsDragging={aButtonIsDragging} />
+      </div>
+    </div>
+  );
 }
