802dfbc395e8286d607333389504ad6fe740f1d7
vpred <vpreduna90@gmail.com>
2021-09-02 14:45:25 -0700

    data, fix filepath to image assets; #15

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,130 +1,172 @@
 const projects = [
-	{
-		key: 11,
-		id: 11,
-		status: 'active',
-		title: 'Checkmarks',
-		description: 'Trademark Registration Application for a local Law Firm, integrated with 3rd party services (Clio, LawPay). React front-end and ASP.NET server-side.',
-		cardImagePath: '../assets/images/projects/Checkmarks.png',
-		cardFileName: '',
-		url: '',
-		techIcons: ['../assets/images/tech/icon-react.png', '../assets/images/tech/icon-aspdotnet.png', '../assets/images/tech/icon-sql.png']
-	},
-	{
-		key: 10,
-		id: 10,
-		status: 'active',
-		title: 'Holistic',
-		description: 'Fitness coach CRM for managing client relationships and schedule. Built with React, using Firestore Authentication and Firestore database.',
-		cardImagePath: '../assets/images/projects/Holistic.jpg',
-		cardFileName: '',
-		url: '',
-		techIcons: ['../assets/images/tech/icon-react.png', '../assets/images/tech/icon-firebase.png']
-	},
-	{
-		key: 9,
-		id: 9,
-		status: 'active',
-		title: 'Moist-Speak',
-		description: 'Social Media application, built with React and using AWS Lambda, API Gateway and DynamoDB.',
-		cardImagePath: '../assets/images/projects/Vladculator.jpg',
-		cardFileName: '',
-		url: '',
-		techIcons: ['../assets/images/tech/icon-react.png', '../assets/images/tech/icon-nodejs.png', '../assets/images/tech/icon-mongodb.png', '../assets/images/tech/icon-aws.png']
-	},
-	{
-		key: 8,
-		id: 8,
-		status: 'active',
-		title: 'Diamond Hands',
-		description: 'Stock trading app, built with React-Native, consuming Stock Price API, with Firebase Authentication and Firestore database.',
-		cardImagePath: '../assets/images/projects/Vladculator.jpg',
-		cardFileName: '',
-		url: '',
-		techIcons: ['../assets/images/tech/icon-react.png', '../assets/images/tech/icon-firebase.png']
-	},
+  {
+    key: 11,
+    id: 11,
+    status: 'active',
+    title: 'Checkmarks',
+    description:
+      'Trademark Registration Application for a local Law Firm, integrated with 3rd party services (Clio, LawPay). React front-end and ASP.NET server-side.',
+    cardImagePath: '../../assets/images/projects/Checkmarks.jpg',
+    imageFileName: 'Checkmarks.jpg',
+    url: '',
+    techIcons: [
+      '../assets/images/tech/icon-react.png',
+      '../assets/images/tech/icon-aspdotnet.png',
+      '../assets/images/tech/icon-sql.png',
+    ],
+  },
+  {
+    key: 10,
+    id: 10,
+    status: 'active',
+    title: 'Holistic',
+    description:
+      'Fitness coach CRM for managing client relationships and schedule. Built with React, using Firestore Authentication and Firestore database.',
+    cardImagePath: '../../assets/images/projects/Holistic.jpg',
+    imageFileName: 'Holistic.jpg',
+    url: '',
+    techIcons: [
+      '../assets/images/tech/icon-react.png',
+      '../assets/images/tech/icon-firebase.png',
+    ],
+  },
+  {
+    key: 9,
+    id: 9,
+    status: 'active',
+    title: 'Moist-Speak',
+    description:
+      'Social Media application, built with React and using AWS Lambda, API Gateway and DynamoDB.',
+    cardImagePath: '../../assets/images/projects/Vladculator.jpg',
+    imageFileName: 'Vladculator.jpg',
+    url: '',
+    techIcons: [
+      '../assets/images/tech/icon-react.png',
+      '../assets/images/tech/icon-nodejs.png',
+      '../assets/images/tech/icon-mongodb.png',
+      '../assets/images/tech/icon-aws.png',
+    ],
+  },
+  {
+    key: 8,
+    id: 8,
+    status: 'active',
+    title: 'Diamond Hands',
+    description:
+      'Stock trading app, built with React-Native, consuming Stock Price API, with Firebase Authentication and Firestore database.',
+    cardImagePath: '../../assets/images/projects/Vladculator.jpg',
+    imageFileName: 'Vladculator.jpg',
+    url: '',
+    techIcons: [
+      '../assets/images/tech/icon-react.png',
+      '../assets/images/tech/icon-firebase.png',
+    ],
+  },
 
-	{
-		key: 7,
-		id: 7,
-		status: 'active',
-		title: 'WishBucket',
-		description:
-			'Full-Stack App build with ASP.NET and React.js., impleneting User Authentication and CRUD operations with a live database. Users can view, create, edit and delete wishlists and items.',
-		cardImagePath: 'https://i.../assets/images/techur.com/jvOYb3T.jpg',
-		cardFileName: '',
-		url: 'https://wishbucket.netlify.app/',
-		techIcons: ['../assets/images/tech/icon-react.png', '../assets/images/tech/icon-aspdotnet.png', '../assets/images/tech/icon-sql.png']
-	},
-	{
-		key: 6,
-		id: 6,
-		status: 'active',
-		title: 'SHREDZ',
-		description:
-			'Front-end implementation of a fitness hub for interacting with Health & Wellness professionals and their services.',
-		cardImagePath: '../assets/images/projects/Shredz.jpg',
-		cardFileName: 'portfolioItem_shredz.jpg',
-		url: 'https://lucid-hawking-f430e1.netlify.app/',
-		techIcons: ['../assets/images/tech/icon-html5.png', '../assets/images/tech/icon-sass.png', '../assets/images/tech/icon-javascript.png']
-	},
-	{
-		key: 5,
-		id: 5,
-		status: 'active',
-		title: 'CHILL',
-		description:
-			"Front-end React.js app which connects to The Movie Database's (TMDb) API",
-		cardImagePath: '../assets/images/projects/Chill.jpg',
-		cardFileName: 'portfolioItem_chill.jpg',
-		url: 'https://xenodochial-wilson-4239db.netlify.app/',
-		techIcons: ['../assets/images/tech/icon-javascript.png', '../assets/images/tech/icon-react.png']
-	},
-	{
-		key: 4,
-		id: 4,
-		status: 'active',
-		title: 'perfectissimo',
-		description:
-			'Interactive menu for a cozy restaurant, with the ability to place orders.',
-		cardImagePath: '../assets/images/projects/Perfectissimo.jpg',
-		cardFileName: '',
-		url: 'https://hopeful-roentgen-612fae.netlify.app/',
-		techIcons: ['../assets/images/tech/icon-javascript.png', '../assets/images/tech/icon-jquery.jpg']
-	},
-	{
-		key: 3,
-		id: 3,
-		status: 'active',
-		title: 'Untamed Romania',
-		description:
-			"A webpage providing an overview of the author's country of origin.",
-		cardImagePath: '../assets/images/projects/UntamedRomania.jpg',
-		cardFileName: '',
-		url: 'https://infallible-hodgkin-d8fc87.netlify.app/',
-		techIcons: ['../assets/images/tech/icon-html5.png', '../assets/images/tech/icon-sass.png', '../assets/images/tech/icon-javascript.png', '../assets/images/tech/icon-jquery.jpg']
-	},
-	{
-		key: 2,
-		id: 2,
-		status: 'active',
-		title: 'Bears of Bad News',
-		description: 'A webpage for a local news paper.',
-		cardImagePath: '../assets/images/projects/BOBN.jpg',
-		cardFileName: '',
-		url: 'https://pensive-jepsen-f44ba1.netlify.app',
-		techIcons: ['../assets/images/tech/icon-html5.png', '../assets/images/tech/icon-bootstrap.png']
-	},
-	{
-		key: 1,
-		id: 1,
-		status: 'active',
-		title: 'Vladculator',
-		description: 'A simple calculator application in C#.',
-		cardImagePath: '../assets/images/projects/Vladculator.jpg',
-		cardFileName: '',
-		url: '',
-		techIcons: ['../assets/images/tech/icon-csharp.png']
-	},
+  {
+    key: 7,
+    id: 7,
+    status: 'active',
+    title: 'WishBucket',
+    description:
+      'Full-Stack App build with ASP.NET and React.js., impleneting User Authentication and CRUD operations with a live database. Users can view, create, edit and delete wishlists and items.',
+    cardImagePath: '../../assets/images/projects/WishBucket.jpg',
+    imageFileName: 'WishBucket.jpg',
+    url: 'https://wishbucket.netlify.app/',
+    techIcons: [
+      '../assets/images/tech/icon-react.png',
+      '../assets/images/tech/icon-aspdotnet.png',
+      '../assets/images/tech/icon-sql.png',
+    ],
+  },
+  {
+    key: 6,
+    id: 6,
+    status: 'active',
+    title: 'SHREDZ',
+    description:
+      'Front-end implementation of a fitness hub for interacting with Health & Wellness professionals and their services.',
+    cardImagePath: '../../assets/images/projects/Shredz.jpg',
+    imageFileName: 'Shredz.jpg',
+    url: 'https://lucid-hawking-f430e1.netlify.app/',
+    techIcons: [
+      '../assets/images/tech/icon-html5.png',
+      '../assets/images/tech/icon-sass.png',
+      '../assets/images/tech/icon-javascript.png',
+    ],
+  },
+  {
+    key: 5,
+    id: 5,
+    status: 'active',
+    title: 'CHILL',
+    description:
+      "Front-end React.js app which connects to The Movie Database's (TMDb) API",
+    cardImagePath: '../../assets/images/projects/Chill.jpg',
+    imageFileName: 'Chill.jpg',
+    url: 'https://xenodochial-wilson-4239db.netlify.app/',
+    techIcons: [
+      '../assets/images/tech/icon-javascript.png',
+      '../assets/images/tech/icon-react.png',
+    ],
+  },
+  {
+    key: 4,
+    id: 4,
+    status: 'active',
+    title: 'perfectissimo',
+    description:
+      'Interactive menu for a cozy restaurant, with the ability to place orders.',
+    cardImagePath: '../../assets/images/projects/Perfectissimo.jpg',
+    imageFileName: 'Perfectissimo.jpg',
+    url: 'https://hopeful-roentgen-612fae.netlify.app/',
+    techIcons: [
+      '../assets/images/tech/icon-javascript.png',
+      '../assets/images/tech/icon-jquery.jpg',
+    ],
+  },
+  {
+    key: 3,
+    id: 3,
+    status: 'active',
+    title: 'Untamed Romania',
+    description:
+      "A webpage providing an overview of the author's country of origin.",
+    cardImagePath: '../../assets/images/projects/UntamedRomania.jpg',
+    imageFileName: 'UntamedRomania.jpg',
+    url: 'https://infallible-hodgkin-d8fc87.netlify.app/',
+    techIcons: [
+      '../assets/images/tech/icon-html5.png',
+      '../assets/images/tech/icon-sass.png',
+      '../assets/images/tech/icon-javascript.png',
+      '../assets/images/tech/icon-jquery.jpg',
+    ],
+  },
+  {
+    key: 2,
+    id: 2,
+    status: 'active',
+    title: 'Bears of Bad News',
+    description: 'A webpage for a local news paper.',
+    cardImagePath: '../../assets/images/projects/BOBN.jpg',
+    // cardImagePath: '../assets/images/projects/BOBN.jpg',
+    imageFileName: 'BOBN.jpg',
+    url: 'https://pensive-jepsen-f44ba1.netlify.app',
+    techIcons: [
+      '../assets/images/tech/icon-html5.png',
+      '../assets/images/tech/icon-bootstrap.png',
+    ],
+  },
+  {
+    key: 1,
+    id: 1,
+    status: 'active',
+    title: 'Vladculator',
+    description: 'A simple calculator application in C#.',
+    cardImagePath: '../assets/images/projects/Vladculator.jpg',
+    imageFileName: 'Vladculator.jpg',
+    url: '',
+    techIcons: ['../assets/images/tech/icon-csharp.png'],
+  },
 ];
 export default projects;
