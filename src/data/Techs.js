af84f3b874479a0be9a3478dbd4b4e97c02667e5
vpred <vpreduna90@gmail.com>
2021-09-03 01:54:46 -0700

    add Techs.js, tech data obj; #4

---
+++ src/data/Techs.js
@@ -0,0 +1,89 @@
+const techs = [
+  {
+    name: 'HTML5',
+    type: 'front-end',
+    imageFilePath: require('../assets/images/tech/icon-html5.png'),
+  },
+  {
+    name: 'CSS',
+    type: 'front-end',
+    imageFilePath: require('../assets/images/tech/icon-css.png'),
+  },
+  {
+    name: 'SASS',
+    type: 'front-end',
+    imageFilePath: require('../assets/images/tech/icon-sass.png'),
+  },
+  {
+    name: 'Javascript',
+    type: 'front-end',
+    imageFilePath: require('../assets/images/tech/icon-javascript.png'),
+  },
+  {
+    name: 'React',
+    type: 'framework',
+    imageFilePath: require('../assets/images/tech/icon-react.png'),
+  },
+  {
+    name: 'JQuery',
+    type: 'framework',
+    imageFilePath: require('../assets/images/tech/icon-jquery.jpg'),
+  },
+  {
+    name: 'Bootstrap',
+    type: 'framework',
+    imageFilePath: require('../assets/images/tech/icon-bootstrap.png'),
+  },
+  {
+    name: 'ASP.NET',
+    type: 'framework',
+    imageFilePath: require('../assets/images/tech/icon-aspdotnet.png'),
+  },
+  {
+    name: 'SQL',
+    type: 'database',
+    imageFilePath: require('../assets/images/tech/icon-sql.png'),
+  },
+  {
+    name: 'Mongo',
+    type: 'database',
+    imageFilePath: require('../assets/images/tech/icon-mongodb.png'),
+  },
+  {
+    name: 'NodeJS',
+    type: 'back-end',
+    imageFilePath: require('../assets/images/tech/icon-nodejs.png'),
+  },
+  {
+    name: 'C-Sharp',
+    type: 'back-end',
+    imageFilePath: require('../assets/images/tech/icon-csharp.png'),
+  },
+  {
+    name: 'Firebase',
+    type: 'cloud services',
+    imageFilePath: require('../assets/images/tech/icon-firebase.png'),
+  },
+  {
+    name: 'Amazon Web Services',
+    type: 'cloud services',
+    imageFilePath: require('../assets/images/tech/icon-aws.png'),
+  },
+  {
+    name: 'AWS Lambda',
+    type: 'cloud services',
+    imageFilePath: require('../assets/images/tech/icon-aws-lambda.png'),
+  },
+  {
+    name: 'AWS API Gateway',
+    type: 'cloud services',
+    imageFilePath: require('../assets/images/tech/icon-aws-apigateway.png'),
+  },
+  {
+    name: 'AWS DynamoDB',
+    type: 'cloud services',
+    imageFilePath: require('../assets/images/tech/icon-aws-dynamodb.png'),
+  },
+];
+
+export default techs;
