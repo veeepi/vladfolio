a117857997a0fa031b6fccee9ff8774e490ab380
vpred <vpreduna90@gmail.com>
2021-09-05 22:56:38 -0700

    TechCheckbox, handleCheckbox function; #22

--- src/components/custom/TechCheckbox.js
+++ src/components/custom/TechCheckbox.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
-export default function TechCheckbox({ tech }) {
+export default function TechCheckbox({
+  tech,
+  techSelected,
+  selectedTechs,
+  setSelectedTechs,
+}) {
+  const handleTechCheckbox = () => {
+    let newTechsSelected = [];
+    if (techSelected) {
+      selectedTechs.forEach((element) => {
+        if (element.name !== tech.name) {
+          newTechsSelected.push(element);
+        }
+      });
+    } else {
+      newTechsSelected = selectedTechs;
+      newTechsSelected.push(tech);
+    }
+    setSelectedTechs(newTechsSelected);
+  };
+
   return (
     <div className="portfolioPage__filter__form__item">
       <img src={tech.imageFilePath.default} alt={tech.name} height="20px" />
       <input
         type="checkbox"
-        //   id="frontEnd"
-        //   name="portfolioItemType"
-        //   value="true"
-        //   defaultChecked
+        checked={techSelected}
+        onChange={handleTechCheckbox}
         className="portfolioPage__filter__form__item__input"
       ></input>
     </div>
