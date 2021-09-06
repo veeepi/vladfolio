793a9607e449cb1841fd6169cbfb2519369120b1
vpred <vpreduna90@gmail.com>
2021-09-05 22:58:23 -0700

    PortfolioPage, render TechCheckboxes; #22

--- src/components/panel/pages/PortfolioPage.js
+++ src/components/panel/pages/PortfolioPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import projects from '../../../data/Projects';
 import techs from '../../../data/Techs';
 import Tile from '../../custom/Tile';
@@ -6,7 +6,13 @@ import TechCheckbox from '../../custom/TechCheckbox';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 export default function PortfolioPage() {
-  // const bodyHeight = window.innerHeight >
+  const [selectedTechs, setSelectedTechs] = useState(techs);
+  const [displayedProjects, setDisplayedProjects] = useState(projects);
+  useEffect(() => {
+    // update displayedProjects, when selectedTechs change
+  }, [selectedTechs]);
+
+  console.log('selectedTechs: ', selectedTechs);
 
   return (
     <div className="portfolioPage">
@@ -14,14 +20,28 @@ export default function PortfolioPage() {
         <button>Expand</button>
         <form className="portfolioPage__filter__form">
           {techs.map((item, index) => {
-            return <TechCheckbox tech={item} key={index} />;
+            let techSelected = false;
+            selectedTechs.forEach((element) => {
+              if (item.name === element.name) {
+                techSelected = true;
+              }
+            });
+            return (
+              <TechCheckbox
+                tech={item}
+                key={index}
+                techSelected={techSelected}
+                selectedTechs={selectedTechs}
+                setSelectedTechs={setSelectedTechs}
+              />
+            );
           })}
         </form>
       </div>
 
       <Scrollbars style={{ height: '79vh' }} className="portfolioPage__body">
         <div className="portfolioPage__body__tiles">
-          {projects.map((item, index) => {
+          {displayedProjects.map((item, index) => {
             return <Tile project={item} key={index} />;
           })}
         </div>
