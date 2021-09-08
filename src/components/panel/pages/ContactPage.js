78bc97f38ffce4a3559760fe88d1eb1dc363d0d1
vpred <vpreduna90@gmail.com>
2021-09-08 00:51:58 -0700

    styling: contact page; #31

--- src/components/panel/pages/ContactPage.js
+++ src/components/panel/pages/ContactPage.js
@@ -11,61 +11,67 @@ export default function ContactPage() {
 
   return (
     <div className="contactPage">
-      <form className="contactPage__form" onSubmit={handleSubmit}>
-        <div className="contactPage__form__section">
-          <label className="contactPage__form__section__label">Name: </label>
-          <input
-            required
-            className="contactPage__form__section__input"
-            type="text"
-            value={contact.name}
-            onChange={(e) =>
-              setContact({
-                ...contact,
-                name: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div className="contactPage__form__section">
-          <label className="contactPage__form__section__label">
-            Email Address:{' '}
-          </label>
-          <input
-            required
-            className="contactPage__form__section__input"
-            type="email"
-            value={contact.email}
-            onChange={(e) =>
-              setContact({
-                ...contact.ContactPage,
-                email: e.target.value,
-              })
-            }
-          />
-        </div>
-        <div className="contactPage__form__section">
-          <label className="contactPage__form__section__label">Message: </label>
-          <textarea
-            rows="4"
-            cols="50"
-            wrap="hard"
-            id="messageBox"
-            className="contactPage__form__section__input"
-            value={contact.message.message}
-            onChange={(e) =>
-              setContact({
-                ...contact,
-                message: e.target.value,
-              })
-            }
-          />
-        </div>
+      <div className="contactPage__section">
+        <form className="contactPage__section__form" onSubmit={handleSubmit}>
+          <div className="contactPage__section__form__field">
+            <label className="contactPage__section__form__field__label">
+              Name:{' '}
+            </label>
+            <input
+              required
+              className="contactPage__section__form__field__input"
+              type="text"
+              value={contact.name}
+              onChange={(e) =>
+                setContact({
+                  ...contact,
+                  name: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div className="contactPage__section__form__field">
+            <label className="contactPage__section__form__field__label">
+              Email Address:{' '}
+            </label>
+            <input
+              required
+              className="contactPage__section__form__field__input"
+              type="email"
+              value={contact.email}
+              onChange={(e) =>
+                setContact({
+                  ...contact.ContactPage,
+                  email: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div className="contactPage__section__form__field">
+            <label className="contactPage__section__form__field__label">
+              Message:{' '}
+            </label>
+            <textarea
+              rows="4"
+              cols="50"
+              wrap="hard"
+              id="messageBox"
+              className="contactPage__section__form__field__input"
+              value={contact.message.message}
+              onChange={(e) =>
+                setContact({
+                  ...contact,
+                  message: e.target.value,
+                })
+              }
+            />
+          </div>
 
-        <button className="contactPage__form__submit" type="submit">
-          Submit
-        </button>
-      </form>
+          <button className="contactPage__section__form__submit" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
       {/* <div className="contactPage__social">
 					<AiOutlineMail size={36} className="contactPage__social__icon"/>
 					<AiFillLinkedin size={36} className="contactPage__social__icon" />
