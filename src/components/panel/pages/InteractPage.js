406809207732d1484ad1eaefda1a0021d805ffd9
vpred <vpreduna90@gmail.com>
2021-10-07 22:26:27 -0700

    Post, onClick select app; #44

--- src/components/panel/pages/InteractPage.js
+++ src/components/panel/pages/InteractPage.js
@@ -1,46 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import posts from '../../../data/Posts';
+// import Tile from '../../custom/Tile';
 
 export default function InteractPage() {
+  const [appSelected, setAppSelected] = useState(null);
+
   return (
     <div className="interactPage animate__animated animate__bounceInLeft">
-      {/* Coming Soon */}
-      {/* Make Fontsize individual letters reactive on Hover */}
-      {/* Infinite Transition in Color? cycle between 4 */}
-      {/* Display a piece of data somehow */}
+      {appSelected ? (
+        <div>App selected</div>
+      ) : (
+        <div className="interactPage__menu">
+          {posts.map((item, index) => {
+            return (
+              // <Tile project={item} key={index} />
 
-      <h1 className="interactPage__heading">
-        <span className="interactPage__heading__letters">
-          <span id="1C">C</span>
-          <span id="2O">O</span>
-          <span id="3M">M</span>
-          <span id="4I">I</span>
-          <span id="5N">N</span>
-          <span id="6G">G</span>
-          <span id="7blank"> </span>
-          <span id="8S">S</span>
-          <span id="91">O</span>
-          <span id="11O">O</span>
-          <span id="12N">N</span>
-        </span>
-        <br />
-        {/* <IoConstructOutline
-						size={42}
-						className="interactPage__heading__icon"
-					/> */}
-      </h1>
-      {/* <div className="interactPage__body">
-        {posts.map((item, index) => {
-          return (
-            <div key={index} className="interactPage__body__post">
-              <h1 className="interactPage__body__post__title">{item.title}</h1>
-              <p className="interactPage__body__post__description">
-                {item.description}
-              </p>
-            </div>
-          );
-        })}
-      </div> */}
+              <button
+                key={index}
+                className="interactPage__menu__post"
+                onClick={() => setAppSelected(item.id)}
+              >
+                <h1 className="interactPage__menu__post__title">
+                  {item.title}
+                </h1>
+                <p className="interactPage__menu__post__description">
+                  {item.description}
+                </p>
+              </button>
+            );
+          })}
+          <h1 className="interactPage__heading">
+            <span className="interactPage__heading__letters">
+              <span>More</span>
+              <span id="7blank"> </span>
+              <span id="1C">C</span>
+              <span id="2O">O</span>
+              <span id="3M">M</span>
+              <span id="4I">I</span>
+              <span id="5N">N</span>
+              <span id="6G">G</span>
+              <span id="7blank"> </span>
+              <span id="8S">S</span>
+              <span id="91">O</span>
+              <span id="11O">O</span>
+              <span id="12N">N</span>
+            </span>
+          </h1>
+        </div>
+      )}
     </div>
   );
 }
